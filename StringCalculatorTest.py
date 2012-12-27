3258e5f665111f7aba232e8505b84c6c7a8d1bf4
matason <hello@webgoodness.co.uk>
2012-12-27 22:06:10 +0000

    Added test: When I add 1, then I should get 1

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -5,5 +5,9 @@ class TestStringCalculator(unittest.TestCase):
   def test_add_empty_string(self):
     self.assertEqual(add(""), 0)
 
+  def test_add_single_number(self):
+    self.assertEqual(add("1"), 1)
+
+
 if __name__ == '__main__':
   unittest.main()
