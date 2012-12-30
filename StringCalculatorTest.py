818fbea773b380919abe6f3d028c9d8a3851a470
matason <hello@webgoodness.co.uk>
2012-12-30 17:07:43 +0000

    Added test: When I add comma separated values

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -8,6 +8,8 @@ class TestStringCalculator(unittest.TestCase):
   def test_add_single_number(self):
     self.assertEqual(add("1"), 1)
 
+  def test_add_comma_separated_numbers(self):
+    self.assertEqual(add("1,2"), 3)
 
 if __name__ == '__main__':
   unittest.main()
