8de33dba6500d283ee020113eacf457909e8e4f7
matason <hello@webgoodness.co.uk>
2012-12-30 21:35:01 +0000

    Test added: When I pass an unknown number of arguments

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -11,5 +11,9 @@ class TestStringCalculator(unittest.TestCase):
   def test_add_comma_separated_numbers(self):
     self.assertEqual(add("1,2"), 3)
 
+  def test_unknown_number_of_arguments(self):
+    self.assertEqual(add("1,2", "4,5"), 12)
+    self.assertEqual(add("1,2", "4,5", ""), 12)
+
 if __name__ == '__main__':
   unittest.main()
