05cfd648bec18da98aa7f362790f1b4ba9f64921
matason <chris.maiden@ymail.com>
2012-12-31 16:42:42 +0000

    Refactored, added a separate function for normalising the separator

--- StringCalculator.py
+++ StringCalculator.py
@@ -2,8 +2,11 @@ def add(*arguments):
   total = 0
 
   for arg in arguments:
-    arg = arg.replace("\n", ",")
+    arg = normaliseSeparator(arg)
     if arg != "":
       total += sum(map(int, arg.split(",")))
 
   return total
+
+def normaliseSeparator(numbers):
+  return numbers.replace("\n", ",")
