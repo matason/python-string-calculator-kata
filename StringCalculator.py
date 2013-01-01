92047b2f3e797afd71258880aaf3408cd9c5cd60
matason <chris.maiden@ymail.com>
2013-01-01 19:53:00 +0000

    Detect and replace supplied separators

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,9 +1,12 @@
+import re
+
 SEPARATOR = ","
 
 def add(*arguments):
   total = 0
 
   for arg in arguments:
+    arg = normaliseSuppliedSeparator(arg)
     arg = normaliseSeparator(arg)
     if arg != "":
       total += sum(map(int, arg.split(SEPARATOR)))
@@ -12,3 +15,11 @@ def add(*arguments):
 
 def normaliseSeparator(numbers):
   return numbers.replace("\n", SEPARATOR)
+
+def normaliseSuppliedSeparator(numbers):
+  if re.match("//.{1}\n", numbers):
+    separator = numbers[2]
+    numbers = numbers[4:]
+    numbers = numbers.replace(separator, SEPARATOR)
+
+  return numbers
