37de0af8c5c7a96bb7155cd37fa2484279339d64
matason <chris.maiden@ymail.com>
2013-01-01 17:58:27 +0000

    Refactored, removed duplication

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,12 +1,14 @@
+SEPARATOR = ","
+
 def add(*arguments):
   total = 0
 
   for arg in arguments:
     arg = normaliseSeparator(arg)
     if arg != "":
-      total += sum(map(int, arg.split(",")))
+      total += sum(map(int, arg.split(SEPARATOR)))
 
   return total
 
 def normaliseSeparator(numbers):
-  return numbers.replace("\n", ",")
+  return numbers.replace("\n", SEPARATOR)
